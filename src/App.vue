f3682bc92f9d85ce87c01bcdaaca61e1ea49a675
SRP <siriphicha.m@gmail.com>
2025-04-04 20:11:16 +0700

    fix/ change size,font, curser,

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,10 @@
 import { ref, onMounted, onUnmounted } from 'vue';
 
 const colorList = ref([
-  { color: '#b8f2ff' },
-  { color: '#558be0' },
-  { color: 'purple' },
-  { color: '#021433' }
+  { color: '#CBEFFF' },
+  { color: '#1A64FC' },
+  { color: '#6700F0' },
+  { color: '#181448' }
 ]);
 
 const selectedColor = ref('');
@@ -35,12 +35,14 @@ onUnmounted(() => {
     <div class="box">
       <button class="colored-box" :class="{ 'red-outline': showColorSelect }"
         :style="{ backgroundColor: selectedColor }" @click="toggleColorSelect"></button>
-      <h1>Space & Winter</h1>
-      <h3>Sleek and Modern</h3>
+      <div style="width: 100%;">
+        <h1>Space Winter</h1>
+        <h2>Modern & Sleek</h2>
+      </div>
     </div>
 
     <div v-if="showColorSelect" class="color-select box">
-      <h3>Select Color</h3>
+      <h3>Select Color :</h3>
       <div class="color-list">
         <button v-for="color in colorList" :key="color.color" @click="selectedColor = color.color" class="color-each"
           :style="{ backgroundColor: color.color }">
@@ -51,62 +53,70 @@ onUnmounted(() => {
 </template>
 
 <style lang="css">
+button:hover {
+  cursor: pointer;
+}
+
 .page-container {
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  gap: 20px;
+  gap: 10px;
+  width: 100%;
 }
 
 .box {
   background: #FFFFFF;
-  box-shadow: 0px 10px 0px #0000003c;
+  box-shadow: 0px 6px 0px #0000003c;
   border-radius: 1rem;
   color: black !important;
   display: flex;
   flex-direction: column;
   align-items: center;
   text-align: center;
-  padding: 1.2rem 2rem;
-  gap: 5px;
+  padding: 1.3rem 1.3rem;
+  gap: 10px;
 
   width: 100%;
   max-width: 1000px;
 }
 
+
 .colored-box {
-  border-radius: 1rem;
+  border-radius: 0.5rem;
   width: 100%;
   aspect-ratio: 1 / 1;
   border: none;
-  margin: 10px;
+}
+
+.color-list {
+  width: 100%;
 }
 
 .color-list .colored-box:focus {
-  outline: red 2px solid;
+  outline: red 1px solid;
 }
 
 .red-outline {
-  outline: red 2px solid;
+  outline: red 1px solid;
 }
 
 .color-select .color-list {
   display: flex;
   flex-wrap: wrap;
-  gap: 1rem;
-  justify-content: center;
-  max-width: calc(40px * 4 + 1rem * 3);
+  width: 100%;
+  justify-content: space-between;
 }
 
 .color-each {
   border-radius: 0.5rem;
-  width: 40px;
+  width: 60px;
   aspect-ratio: 1 / 1;
   border: none;
 }
 
 .color-each:focus {
-  outline: 2px solid red;
+  border: 1px solid red;
 }
 </style>
